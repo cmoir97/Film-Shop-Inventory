2497df68b774167ff762f265321748db2db8ed8c
Cameron Moir <cameronjmoir@gmail.com>
2019-10-05 13:58:34 +0100

    film shop sql file set up

---
+++ film_shop/db/film_shop.sql
@@ -0,0 +1,18 @@
+DROP TABLE films;
+DROP TABLE production_companies;
+
+CREATE TABLE production_companies
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE films
+(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  quantity INT8,
+  buy_cost INT8,
+  sell_price INT8,
+  production_company_id INT8 REFERENCES production_companies(id)
+);
