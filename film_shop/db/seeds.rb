3eda2c0ad2141a6dabc94dfbd1388ad136c19159
Cameron Moir <cameronjmoir@gmail.com>
2019-10-05 15:53:12 +0100

    added delete function to film class

--- film_shop/db/seeds.rb
+++ film_shop/db/seeds.rb
@@ -10,7 +10,10 @@ production_company1.save()
 
 film1 = Film.new({'title' => 'There Will be Blood', 'director' => 'Paul Thomas Anderson', 'quantity' => 10, 'sell_price' => 1000, 'production_company_id' => production_company1.id()})
 
+film2 = Film.new({'title' => 'Blue Velvet', 'director' => 'David Lynch', 'quantity' => 15, 'sell_price' => 500, 'production_company_id' => production_company1.id()})
+
 film1.save()
+film2.save()
 
 
 
